bca40235f691e21fafac91b7ee8499af4a0a6368
acerjak <62491401+acerjak@users.noreply.github.com>
2020-05-17 22:11:42 -0700

    file updates to reflect requirements

---
+++ app.js
@@ -0,0 +1,70 @@
+const prompt = require('inquirer')
+
+//function for member info
+class Employee {
+    constructor (name, id, title) {
+        this.name = name
+        this.id = id
+        this.title = title
+        this.getName = () => {
+            
+        }
+        this.getId = () => {
+
+        }
+        this.getEmail = () => {
+
+        }
+        this.getRole = () => {
+            returns 'Employee'
+        }
+    }
+}
+
+//function for manager w/member info
+class Manager extends Employee {
+    constructor (name, id, title, officeNumber) {
+        super(name, id, title)
+        this.officeNumber = officeNumber
+        this.getRole = () => {
+            return 'Manager'
+        }
+    }
+}
+
+// variable for manager test
+let manager = new Manager('Amanda','amanda@gmail.com','acerjak')
+
+//function for engineer w/member info
+class Engineer extends Employee {
+    constructor (name, id, title, github) {
+        super(name, id, title)
+        this.github = github
+        this.getGithub = () => {
+
+        }
+        this.getRole = () => {
+            return 'Engineer'
+        }
+    }
+}
+
+//function for intern w/member info
+class Intern extends Employee {
+    constructor (name, id, title, school) {
+        super(name, id, title)
+        this.school = school
+        this.getSchool = () => {
+
+        }
+        this.getRole = () => {
+            return 'Intern'
+        }
+    }
+}
+
+module.exports = Employee
+module.exports = Manager
+module.exports = Engineer
+module.exports = Intern
+
---
+++ tests/Manager.test.js
@@ -0,0 +1,5 @@
+const Manager = require('./index.js')
+
+test('Run addManager and return manager info.', () => {
+    expect(Manager()).toEqual(manager)
+})
