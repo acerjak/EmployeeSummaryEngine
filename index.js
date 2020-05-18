ab2cb45d050a67f61b4f82c62a78299d0562fe31
acerjak <62491401+acerjak@users.noreply.github.com>
2020-05-17 20:34:20 -0700

    created basic structure of future webpage

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const prompt = require('inquirer')
+
+
+
+
+
+
+
+
+
+
+
+
+//to be added at the top when prompted
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>My Team</title>
+//     <!-- Compiled and minified CSS -->
+//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+// </head>
+
+
+
+
+//to be added at bottom of page when all is rendered
+// <body>
+//     <!-- Compiled and minified JavaScript -->
+//     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+// </body>
+// </html>
