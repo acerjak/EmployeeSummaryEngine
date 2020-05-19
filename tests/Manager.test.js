0f95cec3169dd9fb020b20635f0eca602abf6499
acerjak <62491401+acerjak@users.noreply.github.com>
2020-05-18 22:10:36 -0700

    16/16 tests pass

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -3,9 +3,31 @@ const Employee = require('../lib/Employee.js')
 //tie in Manager.js
 const Manager = require('../lib/Manager.js')
 
+//test to retrive officeNumber
+test("Can set office number via constructor argument", () => {
+    const officeNumValue = 100;
+    const employee = new Manager("Foo", 1, "test@test.com", officeNumValue);
+    expect(employee.officeNumber).toBe(officeNumValue);
+  });
+
 //test for specific role: Manager
 test("Run getRole and return specific role", () => {
     const title = "Manager"
     const manager = new Manager('Amanda', 1, 'email@email.com')
     expect(manager.getRole()).toBe(title)
 })
+
+
+  
+//   test("getRole() should return \"Manager\"", () => {
+//     const testValue = "Manager";
+//     const e = new Manager("Foo", 1, "test@test.com", 100);
+//     expect(e.getRole()).toBe(testValue);
+//   });
+  
+//   test("Can get office number via getOffice()", () => {
+//     const testValue = 100;
+//     const e = new Manager("Foo", 1, "test@test.com", testValue);
+//     expect(e.getOfficeNumber()).toBe(testValue);
+//   });
+  
