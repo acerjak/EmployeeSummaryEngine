c26e7954fc47738d419c8e7fb9453b8c002ed747
acerjak <62491401+acerjak@users.noreply.github.com>
2020-05-18 22:28:19 -0700

    updated readme with some details

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# EmployeeSummaryEngine
+## EmployeeSummaryEngine
+## Description
+> 
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+#### Please follow this command to install npm dependencies:
+> npm init -y
+> npm i inquirer
+> npm i jest
+## Usage
+> 
+## Credits
+> 
+## License
+> None
+## Tests
+> This application is tested with jest 
+> Run the following command to run:
+> npm run test
+## Questions
+> Please send any inquiries or concerns through [acerjak](https://api.github.com/users/acerjak "GitHub Profile") or at amandacerjak@gmail.com.
+
+![profilepic](https://avatars1.githubusercontent.com/u/62491401?v=4 "acerjak")
+***
+#### Created by acerjak May 2020
--- app.js
+++ app.js
@@ -1,70 +1,16 @@
-// const prompt = require('inquirer')
+//bring in inquirer
+const prompt = require('inquirer')
 
-// //function for member info
-// class Employee {
-//     constructor (name, id, title) {
-//         this.name = name
-//         this.id = id
-//         this.title = title
-//         this.getName = () => {
-            
-//         }
-//         this.getId = () => {
+//bring in class constructors from other js files
+const Employee = require('../lib/Employee.js')
+const Manager = require('../lib/Manager.js')
+const Engineer = require('../lib/Engineer.js')
+const Intern = require('../lib/Intern.js')
 
-//         }
-//         this.getEmail = () => {
-
-//         }
-//         this.getRole = () => {
-//             return 'Employee'
-//         }
-//     }
-// }
-// variable for manager test
-// let employee = new Employee('Amanda','12345','engineer')
-
-
-//function for manager w/member info
-// class Manager extends Employee {
-//     constructor (name, id, title, officeNumber) {
-//         super(name, id, title)
-//         this.officeNumber = officeNumber
-//         this.getRole = () => {
-//             return 'Manager'
-//         }
-//     }
-// }
-
-// // function for engineer w/member info
-// class Engineer extends Employee {
-//     constructor (name, id, title, github) {
-//         super(name, id, title)
-//         this.github = github
-//         this.getGithub = () => {
-
-//         }
-//         this.getRole = () => {
-//             return 'Engineer'
-//         }
-//     }
-// }
-
-//function for intern w/member info
-// class Intern extends Employee {
-//     constructor (name, id, title, school) {
-//         super(name, id, title)
-//         this.school = school
-//         this.getSchool = () => {
-
-//         }
-//         this.getRole = () => {
-//             return 'Intern'
-//         }
-//     }
-// }
-
-// module.exports = Employee
-// module.exports = Manager
-// module.exports = Engineer
-// module.exports = Intern
+prompt ({
+    type: 'list',
+    name: 'buildTeam',
+    message: 'Please choose the type of employee:',
+    choices: 
+})
 
